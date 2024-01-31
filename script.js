930f791d9d585de11e3016029bd7d92cea7316d9
David <amyat@bu.edu>
2024-01-30 19:05:58 -0500

    add buttons and functions
    add button choice, function to generate computer choice, function to generate outcome

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
-    return num
+    
+    if (num == 1) {
+        return "Paper"
+    }
+    else if (num == 2) {
+        return "Rock"
+    }
+    else {
+        return "Scissors"
+    }
+}
+
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    firstLetter = playerSelection.at(0);
+    secondLetter = playerSelection.toLowerCase().slice(1);
+    playerSelection = first + secondLetter;
+    if (playerSelection == getComputerChoice) {
+        return "Tie!"
+    }
+    else if (playerSelection == "rock" && getComputerChoice == "Paper") {
+        return "You Lose! Paper beats Rock";
+    }
+    else if (playerSelection == "Paper" && getComputerChoice == "Rock") {
+        return "You Win! Paper beats Rock";
+    }
+    else if (playerSelection == "Scissors" && getComputerChoice == "Paper") {
+        return "You Win! Scissors beats Paper";
+    }
+    else if (playerSelection == "Paper" && getComputerChoice == "Scissors") {
+        return "You Lose! Paper beats Rock";
+    }
+    else if (playerSelection == "Rock" && getComputerChoice == "Scissors") {
+        return "You Win! Rock beats Scissors";
+    }
+    else {
+        return "You Lose! Rock beats Scissors";
+    }
+
 }
